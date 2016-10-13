168305b74020c1410c9f821d53c0881a6783c962
Rob Thomas <xrobau@gmail.com>
2016-10-13 21:01:50 +0000

    Temporarily remove google and outlook

--- views/rnav.php
+++ views/rnav.php
@@ -7,8 +7,10 @@
 			<li><a href="?display=calendar&amp;action=add&amp;type=local"><i class="fa fa-plus"></i> <strong><?php echo _('Add New Local Calendar')?></strong></a></li>
 			<li><a href="?display=calendar&amp;action=add&amp;type=ical"><i class="fa fa-plus"></i> <strong><?php echo _('Add Remote iCal Calendar')?></strong></a></li>
 			<li><a href="?display=calendar&amp;action=add&amp;type=caldav"><i class="fa fa-plus"></i> <strong><?php echo _('Add Remote CalDAV Calendar')?></strong></a></li>
+<!-- Temporary removal of outlook and google
 			<li><a href="?display=calendar&amp;action=add&amp;type=outlook"><i class="fa fa-plus"></i> <strong><?php echo _('Add Remote Outlook Calendar')?></strong></a></li>
 			<li><a href="?display=calendar&amp;action=add&amp;type=google"><i class="fa fa-plus"></i> <strong><?php echo _('Add Remote Google Calendar')?></strong></a></li>
+-->
 	</ul>
 </div>
  <table id="announcegrid-side" data-url="ajax.php?module=calendar&amp;command=grid" data-maintain-selected="true" data-show-columns="true" data-show-toggle="true" data-toggle="table" data-pagination="true" data-search="true"  id="table-all">
