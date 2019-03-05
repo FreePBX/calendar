b358db836a9524a74ee825a340088bc76ad3e0fb
Andrew Nagy <andrew.nagy@the159.com>
2019-03-04 19:33:19 -0500

    [Module Tag script: calendar 15.0.4.2] Fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -3,16 +3,15 @@ namespace FreePBX\modules\Calendar;
 use FreePBX\modules\Backup as Base;
 
 class Restore extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $settings = $this->getConfigs();
-    foreach ($settings as $key => $value) {
-      $this->FreePBX->Calendar->setMultiConfig($value, $key);
-    }
-  }
+	public function runRestore($jobid){
+		$settings = $this->getConfigs();
+		foreach ($settings as $key => $value) {
+			$this->FreePBX->Calendar->setMultiConfig($value, $key);
+		}
+	}
 
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    return $this->transformLegacyKV($pdo,'calendar', $this->FreePBX)
-                ->transformNamespacedKV($pdo,'calendar', $this->FreePBX);
-  }
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$this->restoreLegacyKvstore($pdo);
+	}
 
 }
