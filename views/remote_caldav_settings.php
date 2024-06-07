28ce5d06b9d001f958caff8e813b9b1e757311b1
Akarsh MR <akarshmr@sangoma.com>
2024-06-07 01:54:39 -0500

    FREEPBX17-828 caldav candar whoops error fix and submit validation added

--- views/remote_caldav_settings.php
+++ views/remote_caldav_settings.php
@@ -258,5 +258,9 @@ if (strtolower((string) $action) == "add") {
 		if($("#urlerror").length == 1) {
 			return warnInvalid($("#url"),_("Please check your URL and credentials."));
 		}
+		if($("#setspan").is(":hidden")) {
+			fpbxToast(_('Validation in progress. Please wait.'),'','warning');
+			return false;
+		}
 	});
 </script>
